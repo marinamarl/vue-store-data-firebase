7a87d24f671a8d7870e935e7a8e592a72744b0f0
Marina Marlag <marina.marlagkoutsou@gmail.com>
2019-02-02 00:46:49 +0200

    added conditional statement to show success message and reload button on sending form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
-  <form class="vue-form" @submit.prevent="submit" v-on:submit.prevent="addUser">
-
+  <form class="vue-form" @submit.prevent="submit" v-on:submit.prevent="addUser" v-if="submitted == false">
     <div class="error-message">
-      <p v-show="!newUser.email.valid">Oh, please enter a valid email address.</p>
+      <p v-show="!newUser.email.valid">Please make sure the email address is valid. Example: someone@somewhere.com.</p>
     </div>
 
     <fieldset>
@@ -12,6 +11,12 @@
         <label class="label" for="name">Name</label>
         <input type="text" name="name" id="name" required="" v-model="newUser.name">
       </div>
+
+      <div>
+        <label class="label" for="lastName">Last Name</label>
+        <input type="text" name="lastName" id="lastName" required="" v-model="newUser.lastName">
+      </div>
+
       <div>
         <label class="label" for="email">Email</label>
         <input type="email" name="email" id="email" required=""
@@ -19,10 +24,28 @@
                v-model="newUser.email.value">
       </div>
       <div>
+        <label class="label" for="phone">Phone Number</label>
+        <input type="text" name="phone" id="phone" required="" v-model="newUser.phone">
+      </div>
+      <div>
+        <label class="label" for="photo">Photo</label>
+        <input type="text" name="photo" id="photo" required="" v-model="newUser.photo">
+      </div>
+
+      <div>
         <input type="submit" value="Send Form">
       </div>
     </fieldset>
+
   </form>
+
+<div class="vue-form" v-else>
+  <legend>Your contact details have been succesfully submitted. Thank you!</legend>
+  <div>
+    <input type="submit" value="Add another person!" onclick="location.reload(true)">
+  </div>
+
+</div>
 
   <div class="debug">
     <pre><code>{{ $data }}</code></pre>
@@ -54,11 +77,14 @@ export default {
   data: function() {
     return {
       newUser: {
-        name: "",
+        name: '',
+        lastName: '',
         email: {
-          value: "",
+          value: '',
           valid: false
-        }
+        },
+        phone: '',
+        Photo: ''
       },
       submitted: false
     };
@@ -70,7 +96,6 @@ export default {
       this.newUser.name = '';
       this.newUser.email.value = '';
     },
-
     // submit form handler
     submit: function() {
       this.submitted = true;
