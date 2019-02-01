51d9b3a95281d76ac5a1193d376aa3635cb9734d
Marina Marlag <marina.marlagkoutsou@gmail.com>
2019-02-01 16:43:37 +0200

    second commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import Firebase from 'firebase'
+let config = {
+    apiKey: "AIzaSyCMDAI881ToJhye3bPCJfjkgw9sc9AU4Ho",
+    authDomain: "contactformvue.firebaseapp.com",
+    databaseURL: "https://contactformvue.firebaseio.com",
+    projectId: "contactformvue",
+    storageBucket: "contactformvue.appspot.com",
+    messagingSenderId: "86941929496"
+  };
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let booksRef = db.ref('books')
+
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
