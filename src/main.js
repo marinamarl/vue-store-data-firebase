db313366e2285c54934a6731cf30356af0906d44
Marina Marlag <marina.marlagkoutsou@gmail.com>
2019-02-01 17:00:28 +0200

    copy-paste code from separate attempt at contact form with vue, and link styles in index.html

--- src/main.js
+++ src/main.js
@@ -12,4 +12,51 @@ new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
+  // the instance state
+  data: function() {
+    return {
+      name: "John Doe",
+      email: {
+        value: "jo@hnd.oe",
+        valid: true
+      },
+      features: ["Reactivity", "Encapsulation", "Data Binding"],
+      selection: {
+        member: "0",
+        framework: "vue",
+        features: []
+      },
+      message: {
+        text: `Dear Mr. President,\n...`,
+        maxlength: 255
+      },
+      submitted: false
+    };
+  },
+  methods: {
+    // submit form handler
+    submit: function() {
+      this.submitted = true;
+    },
+    // validate by type and value
+    validate: function(type, value) {
+      if (type === "email") {
+        this.email.valid = this.isEmail(value) ? true : false;
+      }
+    },
+    // check for valid email adress
+    isEmail: function(value) {
+      return emailRegExp.test(value);
+    },
+    // check or uncheck all
+    checkAll: function(event) {
+      this.selection.features = event.target.checked ? this.features : [];
+    }
+  },
+  watch: {
+    // watching nested property
+    "email.value": function(value) {
+      this.validate("email", value);
+    }
+  }
 })
