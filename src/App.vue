ef16f0b7b859f7f9d73f9205a2045a0afd82a484
Marina Marlag <marina.marlagkoutsou@gmail.com>
2019-02-03 23:53:10 +0200

    markup cleanup done

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,39 @@
 <template>
   <div id="app">
-  <form class="vue-form" @submit.prevent="submit" v-if="submitted == false">
-
-    <fieldset>
-      <legend>Vue Contact Form</legend>
-      // It would be a good idea to have extra validation for each field (not all browsers behave the same with 'required').
-
+    <form class="vue-form" @submit.prevent="submit" v-if="submitted == false">
+      <fieldset>
+        <legend>Vue Contact Form</legend>
+        <!-- It would be a good idea to have extra validation for each field (not all browsers behave the same with 'required'). -->
+        <div>
+          <label class="label" for="name">Name</label>
+          <input type="text" name="name" id="name" required v-model="newUser.name">
+        </div>
+        <div>
+          <label class="label" for="lastName">Last Name</label>
+          <input type="text" name="lastName" id="lastName" required v-model="newUser.lastName">
+        </div>
+        <div>
+          <label class="label" for="email">Email</label>
+          <input type="email" name="email" id="email" required v-model="newUser.email">
+        </div>
+        <div>
+          <label class="label" for="phone">Phone Number</label>
+          <input type="number" name="phone" id="phone" min="0" required v-model="newUser.phone">
+        </div>
+        <div>
+          <input type="file" name="file" id="file" required @change="onFileChanged">
+        </div>
+        <div>
+          <input type="submit" value="Send Form">
+        </div>
+      </fieldset>
+    </form>
+    <div class="vue-form" v-else>
+      <legend>Your contact details have been succesfully submitted. Thank you!</legend>
       <div>
-        <label class="label" for="name">Name</label>
-        <input type="text" name="name" id="name" required v-model="newUser.name">
+        <input type="submit" value="Add another person!" @click="location.reload(true)">
       </div>
-
-      <div>
-        <label class="label" for="lastName">Last Name</label>
-        <input type="text" name="lastName" id="lastName" required v-model="newUser.lastName">
-      </div>
-
-      <div>
-        <label class="label" for="email">Email</label>
-        <input type="email" name="email" id="email" required v-model="newUser.email">
-      </div>
-
-      <div>
-        <label class="label" for="phone">Phone Number</label>
-        <input type="number" name="phone" id="phone" min="0"
-         required v-model="newUser.phone">
-      </div>
-      <div>
-        <input type="file" name="file" id="file" required @change="onFileChanged">
-      </div>
-
-      <div>
-        <input type="submit" value="Send Form">
-      </div>
-    </fieldset>
-
-  </form>
-
-<div class="vue-form" v-else>
-  <legend>Your contact details have been succesfully submitted. Thank you!</legend>
-  <div>
-    <input type="submit" value="Add another person!" onclick="location.reload(true)">
-  </div>
-
-</div>
-
-  <div class="debug">
-    <pre><code>{{ $data }}</code></pre>
-  </div>
+    </div>
   </div>
 </template>
 
